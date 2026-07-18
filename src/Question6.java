87968e3b77411099c35774f2ca97dcb1e4329931
Mohamed Abdelghany <you@example.com>
2026-07-18 06:41:22 +0300

    Add all task files and theory answers

---
+++ src/Question6.java
@@ -0,0 +1,10 @@
+public class Question6 {
+    public static void printTable() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("7 x " + i + " = " + (7 * i));
+        }
+    }
+    public static void main(String[] args) {
+        printTable();
+    }
+}
